378c440b5263c1d8ce3cd5ad998f52441e158ad7
Changpeng Duan <mefistoduan@users.noreply.github.com>
2018-12-12 23:24:18 +0800

    todo

--- src/page/table.vue
+++ src/page/table.vue
@@ -24,11 +24,11 @@
                         </el-col>
                         <el-col :span="4">
                             手机号:
-                            <el-input class="panel_input" v-model="input" placeholder="手机号"></el-input>
+                            <el-input class="panel_input" v-model="input_tel" placeholder="手机号"></el-input>
                         </el-col>
                         <el-col :span="4">
                             爱好:
-                            <el-select class="panel_select" v-model="value4" placeholder="请选择" size="medium">
+                            <el-select class="panel_select" v-model="sel_hobbit" placeholder="请选择" size="medium">
                                 <el-option
                                         v-for="itemSelect in optionsSelect"
                                         :key="itemSelect.value"
@@ -37,8 +37,10 @@
                                 </el-option>
                             </el-select>
                         </el-col>
-                        <el-col :span="3">
-                            <el-button type="primary">查询</el-button>
+                        <el-col :span="6">
+                            <el-button type="primary" @click="query">查询</el-button>
+                            <el-button type="success" @click="change">修改</el-button>
+                            <el-button type="danger" @click="del">删除</el-button>
                         </el-col>
                     </el-row>
                 </div>
@@ -56,49 +58,49 @@
             条记录
         </span>
 
-            <el-table
-                    is-horizontal-resize
-                    style="width:96%"
-                    :data="tableData"
-                    border
-                    :multiple-sort="multipleSort"
-                    @sort-change="sortChange"
-                    row-hover-color="#eee"
-                    row-click-color="#edf7ff"
-                    :default-sort = "{prop: 'date', order: 'descending'}"
-            >
-                    <el-table-column
-                            type="selection"
-                            width="55">
-                    </el-table-column>
-                <el-table-column
-                        label="序号"
-                        sortable
-                        type="index"
-                        width="80">
-                </el-table-column>
-                <el-table-column
-                        prop="name"
-                        label="姓名"
-                        sortable
-                        width="180">
-                </el-table-column>
-                <el-table-column
-                        prop="tel"
-                        label="手机号"
-                        sortable
-                        width="180">
-                </el-table-column>
-                <el-table-column
-                        prop="address"
-                        label="地址"
-                        :formatter="formatter">
-                </el-table-column>
-            </el-table>
+        <el-table
+                is-horizontal-resize
+                style="width:96%"
+                :data="tableData"
+                border
+                :multiple-sort="multipleSort"
+                @sort-change="sortChange"
+                row-hover-color="#eee"
+                row-click-color="#edf7ff"
+                :default-sort="{prop: 'date', order: 'descending'}"
+        >
+            <el-table-column
+                    type="selection"
+                    width="55">
+            </el-table-column>
+            <el-table-column
+                    label="序号"
+                    sortable
+                    type="index"
+                    width="80">
+            </el-table-column>
+            <el-table-column
+                    prop="name"
+                    label="姓名"
+                    sortable
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="tel"
+                    label="手机号"
+                    sortable
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="address"
+                    label="地址"
+                    :formatter="formatter">
+            </el-table-column>
+        </el-table>
 
         </v-table>
         <br>
-        <v-pagination :total="600" @page-change="pageChange1" @page-size-change="pageSizeChange1"></v-pagination>
+        <v-pagination :total="total" @page-change="pageChange"></v-pagination>
     </div>
 </template>
 
@@ -118,20 +120,27 @@
                 value1: '',
                 value2: '',
                 value3: '10',
+                value4: '',
                 value: '10',
                 limit: '10',
-                input: '',
-                optionsSelect: [{
-                    value: '选项1',
+                input_tel: '',
+                sel_hobbit: '全部',
+                total: 0,
+                pageIndex: 1,
+                optionsSelect: [
+                    {
+                    value: '0',
+                    label: '全部'
+                },      {
+                    value: '1',
                     label: '唱歌'
                 }, {
-                    value: '选项2',
+                    value: '2',
                     label: '书法'
                 }, {
-                    value: '选项3',
+                    value: '3',
                     label: '钢琴'
                 }],
-                value4: '',
                 options: [{
                     value: '10',
                     label: '10'
@@ -146,11 +155,6 @@
                     label: '100'
                 },],
                 tableData: [
-                    {"name": "赵伟", "tel": "156123451987", "hobby": "唱歌", "address": "上海市黄浦区金陵东路569号17楼"},
-                    {"name": "李伟", "tel": "182123451538", "hobby": "书法", "address": "上海市奉贤区南桥镇立新路12号2楼"},
-                    {"name": "孙伟", "tel": "161123450097", "hobby": "钢琴", "address": "上海市崇明县城桥镇八一路739号"},
-                    {"name": "周伟", "tel": "197123451123", "hobby": "唱歌", "address": "上海市青浦区青浦镇章浜路24号"},
-                    {"name": "吴伟", "tel": "183123456678", "hobby": "书法", "address": "上海市松江区乐都西路867-871号"}
                 ],
                 multipleSort: false,
                 multipleTable: false,
@@ -190,7 +194,7 @@
                         titleAlign: 'center',
                         columnAlign: 'left',
                         sortable: true,
-                        formatter:"formatter",
+                        formatter: "formatter",
                         isResize: true
                     }
                 ]
@@ -205,35 +209,47 @@
             sortChange(params) {
                 console.log(params)
             },
-            pageChange1(pageIndex) {
-                console.log(pageIndex)
-            },
-            pageSizeChange1(pageSize) {
-                console.log(pageSize)
+            pageChange(pageIndex) {
+                this.pageIndex = pageIndex;
+               this.getTableQuery();
             },
             formatter(row, column) {
                 return row.address;
             },
+            // 查询按钮
+            query(){
+                this.getTableQuery();
+            },
+            // 修改按钮
+            change(){},
+            // 删除按钮
+            del(){
+                // todo
+            },
             getTableQuery() {
-                let $this = this;
-                let url = 'http://localhost:8080/static/api/table.text';//获取
-                let postdata = {};
+                let that = this;
+                let url = 'http://localhost:8080/static/api/table.json';//获取
+                let postdata = {
+                    "bdt":that.value1,
+                    "edt":that.value2,
+                    "tel":that.input_tel,
+                    "hobbit":that.sel_hobbit,
+                    "pageIndex":that.pageIndex,
+                };
                 let JSON = '';
-                axios.get(url,postdata)
+                // 实际生产环境使用post
+                axios.get(url, postdata)
                     .then(function (response) {
-                        JSON = response.data;
-                        // console.log(JSON);
-                        // if (JSON.code == 0) {
-                        //    console.log(JSON);
-                        // } else {
-                        //     console.log(JSON);
-                        // }
+                        JSON =  response.data ;
+                        if (JSON.code == 0) {
+                            that.tableData = JSON.rs;
+                            that.total = JSON.total;
+                        }
                     })
                     .catch(function (error) {
                         console.log(error);
                     });
             }
-
         },
         components: {}
     }
@@ -306,15 +322,19 @@
     .el-select {
         width: 90px;
     }
+
     .panel_input {
         width: 180px;
     }
+
     .panel_data {
         width: 140px;
     }
+
     .panel_select {
         width: 180px;
     }
+
     .panel {
         background: #efefef;
         margin-top: 30px;
