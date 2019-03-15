101bc5d1655f6761c4e77c95542d899ce562959f
Changpeng Duan <838560574@qq.com>
2019-03-15 10:55:22 +0800

    pwd rebuild

--- src/page/pwd.vue
+++ src/page/pwd.vue
@@ -1,88 +1,105 @@
 <template>
     <div id="pages">
-        <ul>
-            <li>
-                <span>原密码:</span>
-                <el-input type="password" v-model="old" placeholder="请输入内容"></el-input>
-            </li>
-            <li>
-                <span>新密码:</span>
-                <el-input type="password" v-model="newpwd" placeholder="请输入内容"></el-input>
-                <s>
-                    <i class="el-icon-warning"></i><em>密码由6~32位字母、数字及下划线组成</em>
-                </s>
-            </li>
-            <li>
-                <span>确认密码:</span>
-                <el-input type="password" v-model="again" placeholder="请输入内容" v-on:blur="blur()"></el-input>
-            </li>
-            <li>
-                <span></span>
-                <el-button type="primary" @click="submitPwd">确认</el-button>
-            </li>
-        </ul>
-    </div>
+            <div class="form_container">
+                <el-form ref="form" :model="form" label-width="110px" :rules="rules">
+                    <el-form-item label="原密码" prop="old">
+                        <el-input v-model="form.old" type="password" show-password></el-input>
+                    </el-form-item>
+                    <el-form-item label="新密码"  prop="newpwd">
+                        <el-input v-model="form.newpwd" type="password" show-password></el-input>
+                    </el-form-item>
+                    <el-form-item label="确认密码"  prop="again">
+                        <el-input v-model="form.again" type="password" show-password></el-input>
+                    </el-form-item>
+                    <s>
+                        <i class="el-icon-warning"></i><em>密码由6~32位字母、数字及下划线组成</em>
+                    </s>
+                    <el-form-item>
+                        <el-button type="primary" @click="onSubmit('form')">确认</el-button>
+                        <el-button @click="resetForm('form')">重置</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </div>
 </template>
 
 <script>
     import axios from 'axios';
     let qs = require('qs');
     import Global from '../Global.js'
+    import validatorPass from '../validatorPass.js'
     export default {
         data() {
+            let samepass = (rule, value, callback) => {
+                 if (value !== this.form.newpwd) {
+                    callback(new Error('两次输入密码不一致!'));
+                } else {
+                    callback();
+                }
+            };
             return {
-                old: '',
-                newpwd: '',
-                again: '',
+                form: {
+                    old:'',
+                    newpwd:'',
+                    again:'',
+                },
+                rules: {
+                    old: [
+                        { required: true, message: '请输入原密码', trigger: 'blur' },
+                        { min: 6, max: 32, message: '长度在 6 到 32 个字符', trigger: 'blur' }
+                    ],
+                    newpwd: [
+                        { required: true, message: '请输入新密码', trigger: 'blur' },
+                        { min: 6, max: 32, message: '长度在 6 到 32 个字符', trigger: 'blur' },
+                        { validator:PwdPass, trigger: 'blur' }
+                    ],
+                    again: [
+                        { required: true, message: '请输入确认密码', trigger: 'blur' },
+                        { min: 6, max: 32, message: '长度在 6 到 32 个字符', trigger: 'blur' },
+                        { validator:PwdPass, trigger: 'blur' },
+                        { validator:samepass, trigger: 'blur' },
+                    ],
+                }
             }
         },
+        mounted() {
+
+        },
         methods: {
-            // 确认密码blur
-            blur() {
-                const that = this;
-                if (that.again != that.newpwd) {
-                    that.$message.error('错了哦，确认密码与新密码不一致');
-                    return false;
-                }
+            onSubmit(formName){
+                let that = this;
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        that.submitPwd()
+                    } else {
+                        that.$message({
+                            showClose: true,
+                            message: '错了哦，提交',
+                            type: 'error'
+                        });
+                        return false;
+                    }
+                });
             },
-            // 密码确认提交
-            submitPwd() {
-                const that = this;
-                if (!that.old) {
-                    that.$message.error('错了哦，原密码不能为空');
-                    return false
-                }
-                if (!that.newpwd) {
-                    that.$message.error('错了哦，新密码不能为空');
-                    return false
-                }
-                if (!that.again) {
-                    that.$message.error('错了哦，确认密码不能为空');
-                    return false
-                }
-                if (that.again != that.newpwd) {
-                    that.$message.error('错了哦，确认密码与新密码不一致');
-                    return false
-                }
-                if (that.newpwd.length > 32) {
-                    that.$message.error('错了哦，密码长度超长');
-                    return false
-                }
-                let re = /^[0-9a-zA-Z_]{1,}$/;
-                if (that.newpwd.search(re) == -1) {
-                    that.$message.error('密码只能由字母、数字及下划线组成');
-                    return false
-                }
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            },
+            submitPwd(){
+                let that = this;
                 let url = headapi + '?ctl=ajax&mod=index&act=modPwd';
                 let param = {
-                    oldPwd: that.old,
-                    newPwd: that.newpwd,
+                    oldPwd: that.form.old,
+                    newPwd: that.form.newpwd,
                 };
                 let postdata = qs.stringify(param);
                 axios.post(url, postdata).then(function (data) {
                     let json = data.data;
                     if (json.code == 0) {
-                        that.$message.success('密码修改成功');
+                        that.$message({
+                            showClose: true,
+                            message: '密码修改成功',
+                            type: 'success'
+                        });
                         // clean info
                         that.old = '';
                         that.newpwd = '';
@@ -94,27 +111,12 @@
                 }, function (response) {
                     console.info(response);
                 })
-
-            },
-            // 重登录
-            logout(){
-                const that = this;
-                let url =  headapi+'?ctl=ajax&mod=index&act=logout';
-                let param = {};
-                let postdata = qs.stringify(param);
-                axios.post(url, postdata).then(function(data){
-                    let json = data.data;
-                    window.top.location.href = '/?act=login#/login?status=1'
-                },function(response){
-                    console.info(response);
-                })
             }
         },
         components: {}
     }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     #pages {
         position: absolute;
@@ -128,54 +130,27 @@
         overflow-y: scroll;
         display: block;
         margin: 0 auto;
-        background-color: #fff;
         padding-bottom: 80px;
     }
-
-    ul {
+    .form_container {
         width: 50%;
         overflow: hidden;
-        float: left;
         margin-top: 20px;
-        margin-left: 20px;
+        margin-left: 40px;
         background: #fff;
         padding: 20px;
     }
-
-    li {
-        width: 100%;
-        overflow: hidden;
-        display: block;
-        margin: 0 auto;
-        list-style: none;
-        height: 50px;
-    }
-
-    li span {
-        width: 60px;
-        float: left;
-        height: 40px;
-        line-height: 40px;
-    }
-
-    /deep/ .el-input {
-        width: 50%;
-        float: left;
-    }
-
-    li s {
+    s {
         height: 50px;
         line-height: 40px;
         padding-left: 20px;
         text-decoration: none;
         font-style: normal;
     }
-
-    li i {
+    i {
         color: red;
     }
-
-    li s em {
+    s em {
         font-style: normal;
     }
 </style>
